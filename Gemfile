634fa27344743b1bca2bb4b8eecfda8fac19d9f5
Aaron Chancey <abchiptop@gmail.com>
2015-07-29 20:31:12 +0000

    added pagination!

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'devise'
 gem 'redcarpet'
 gem 'rouge'
 
+gem 'will_paginate-foundation'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
+require 'will_paginate/array' 
+
 class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   # GET /posts.atom
   def index
-    @posts = Post.published.order('id DESC')
+    @posts = Post.paginate(:page => params[:page], :per_page => 10).published.order('id DESC')
 
     respond_to do |format|
       format.html # index.html.erb
